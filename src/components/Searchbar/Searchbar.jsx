c66508dedbed0470f7c54806919763044a652e50
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-22 11:44:18 +0200

    component SearchBar ==> hook

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import s from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    imageName: '',
-  };
+export default function Searchbar({ onSubmit }) {
+  const [imageName, setImageName] = useState('');
 
-  onSubmitForm = e => {
-    const { imageName } = this.state;
-    const { onSubmit } = this.props;
+  const onSubmitForm = e => {
+    // const { imageName } = this.state;
+    // const { onSubmit } = this.props;
 
     e.preventDefault();
 
@@ -18,40 +16,89 @@ class Searchbar extends Component {
     }
 
     onSubmit(imageName);
-    this.setState({ imageName: '' });
+    // this.setState({ imageName: '' });
+    setImageName('');
   };
 
-  onChangeImageName = ({ target }) => {
-    this.setState({ imageName: target.value });
+  const onChangeImageName = ({ target }) => {
+    // this.setState({ imageName: target.value });
+    setImageName(target.value);
   };
 
-  render() {
-    const { state, onSubmitForm, onChangeImageName } = this;
-    const { imageName } = state;
+  return (
+    <>
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={onSubmitForm}>
+          <button type="submit" className={s.SearchFormButton}>
+            <span className={s.SearchFormButtonLabel}>Search</span>
+          </button>
 
-    return (
-      <>
-        <header className={s.Searchbar}>
-          <form className={s.SearchForm} onSubmit={onSubmitForm}>
-            <button type="submit" className={s.SearchFormButton}>
-              <span className={s.SearchFormButtonLabel}>Search</span>
-            </button>
-
-            <input
-              name="imageName"
-              value={imageName}
-              className={s.SearchFormInput}
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={onChangeImageName}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
+          <input
+            name="imageName"
+            value={imageName}
+            className={s.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={onChangeImageName}
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
-export default Searchbar;
+// class oldSearchbar extends Component {
+//   state = {
+//     imageName: '',
+//   };
+
+//   onSubmitForm = e => {
+//     const { imageName } = this.state;
+//     const { onSubmit } = this.props;
+
+//     e.preventDefault();
+
+//     if (imageName.trim() === '') {
+//       return toast.info('Введите название картинки!');
+//     }
+
+//     onSubmit(imageName);
+//     this.setState({ imageName: '' });
+//   };
+
+//   onChangeImageName = ({ target }) => {
+//     this.setState({ imageName: target.value });
+//   };
+
+//   render() {
+//     const { state, onSubmitForm, onChangeImageName } = this;
+//     const { imageName } = state;
+
+//     return (
+//       <>
+//         <header className={s.Searchbar}>
+//           <form className={s.SearchForm} onSubmit={onSubmitForm}>
+//             <button type="submit" className={s.SearchFormButton}>
+//               <span className={s.SearchFormButtonLabel}>Search</span>
+//             </button>
+
+//             <input
+//               name="imageName"
+//               value={imageName}
+//               className={s.SearchFormInput}
+//               type="text"
+//               autoComplete="off"
+//               autoFocus
+//               placeholder="Search images and photos"
+//               onChange={onChangeImageName}
+//             />
+//           </form>
+//         </header>
+//       </>
+//     );
+//   }
+// }
+
+// export default Searchbar;
