3b1d79d727df0a3d0aac4fbff03843acb85f48a2
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-22 17:57:08 +0200

    component App ==> hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -6,52 +6,95 @@ import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import Modal from './components/Modal/Modal';
 
-class App extends Component {
-  state = {
-    imageName: '',
-    showModal: false,
-    currentImage: '',
-    page: 1,
+export default function App() {
+  const [imageName, setImageName] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [currentImage, setCurrentImage] = useState('');
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    // this.setState(state => ({ showModal: !state.showModal }));
   };
 
-  toggleModal = () => {
-    this.setState(state => ({ showModal: !state.showModal }));
-  };
-
-  onOpenModal = ({ target }) => {
+  const onOpenModal = ({ target }) => {
     if (target.nodeName === 'IMG') {
-      this.setState(({ showModal }) => ({
-        currentImage: target.dataset.image,
-        showModal: !showModal,
-      }));
+      setCurrentImage(target.dataset.image);
+      setShowModal(!showModal);
+      // this.setState(({ showModal }) => ({
+      //   currentImage: target.dataset.image,
+      //   showModal: !showModal,
     }
   };
 
-  onSubmitForm = imageName => {
-    this.setState({ imageName: imageName });
+  const onSubmitForm = imageName => {
+    setImageName(imageName);
+    // this.setState({ imageName: imageName });
   };
 
-  render() {
-    const { state, toggleModal, onSubmitForm, onOpenModal } = this;
-    const { showModal, currentImage, imageName } = state;
-
-    return (
-      <div className="App">
-        {showModal && (
-          <Modal onCloseModal={toggleModal}>
-            <img src={currentImage} alt="" width="950" />
-          </Modal>
-        )}
-        <Searchbar onSubmit={onSubmitForm} />
-        <ImageGallery imageName={imageName} onOpenModal={onOpenModal} />
-        <ToastContainer
-          autoClose={3000}
-          position="bottom-right"
-          transition={Zoom}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {showModal && (
+        <Modal onCloseModal={toggleModal}>
+          <img src={currentImage} alt="" width="950" />
+        </Modal>
+      )}
+      <Searchbar onSubmit={onSubmitForm} />
+      <ImageGallery imageName={imageName} onOpenModal={onOpenModal} />
+      <ToastContainer
+        autoClose={3000}
+        position="bottom-right"
+        transition={Zoom}
+      />
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     imageName: '',
+//     showModal: false,
+//     currentImage: '',
+//     page: 1,
+//   };
+
+//   toggleModal = () => {
+//     this.setState(state => ({ showModal: !state.showModal }));
+//   };
+
+//   onOpenModal = ({ target }) => {
+//     if (target.nodeName === 'IMG') {
+//       this.setState(({ showModal }) => ({
+//         currentImage: target.dataset.image,
+//         showModal: !showModal,
+//       }));
+//     }
+//   };
+
+//   onSubmitForm = imageName => {
+//     this.setState({ imageName: imageName });
+//   };
+
+//   render() {
+//     const { state, toggleModal, onSubmitForm, onOpenModal } = this;
+//     const { showModal, currentImage, imageName } = state;
+
+//     return (
+//       <div className="App">
+//         {showModal && (
+//           <Modal onCloseModal={toggleModal}>
+//             <img src={currentImage} alt="" width="950" />
+//           </Modal>
+//         )}
+//         <Searchbar onSubmit={onSubmitForm} />
+//         <ImageGallery imageName={imageName} onOpenModal={onOpenModal} />
+//         <ToastContainer
+//           autoClose={3000}
+//           position="bottom-right"
+//           transition={Zoom}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
